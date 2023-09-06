90e0fe3dafef43e748e5d25c7897e3e4298fb094
khanMahmood68 <mahmoodashrafkhanm0@gmail.com>
2023-09-06 12:33:57 +0530

    Adding link

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 // Replace 'mongodb://localhost/mydatabase' with your MongoDB connection string
 // const dbURI = ('mongodb://127.0.0.1:27017/Polling_System');
-const dbURI = ('mongodb+srv://mahmood68:HsKmej0cQB7HQJ0p@cluster0.sl5apwz.mongodb.net/')
+const dbURI = ('mongodb+srv://mahmood68:HsKmej0cQB7HQJ0p@cluster0.sl5apwz.mongodb.net/?retryWrites=true&w=majority')
 
+// mongodb+srv://mahmood68:HsKmej0cQB7HQJ0p@cluster0.sl5apwz.mongodb.net/?retryWrites=true&w=majority
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
