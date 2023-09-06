446f3c85f7aa613adf8a1d315db62c118abff1e1
khanMahmood68 <mahmoodashrafkhanm0@gmail.com>
2023-09-06 12:56:55 +0530

    Editing config

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,31 @@
+// const mongoose = require('mongoose');
+
+// // Replace 'mongodb://localhost/mydatabase' with your MongoDB connection string
+// // const dbURI = ('mongodb://127.0.0.1:27017/Polling_System');
+// const dbURI = ('mongodb+srv://mahmood68:HsKmej0cQB7HQJ0p@cluster0.sl5apwz.mongodb.net/?retryWrites=true&w=majority')
+
+// // mongodb+srv://mahmood68:HsKmej0cQB7HQJ0p@cluster0.sl5apwz.mongodb.net/?retryWrites=true&w=majority
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
+
 const mongoose = require('mongoose');
 
-// Replace 'mongodb://localhost/mydatabase' with your MongoDB connection string
-// const dbURI = ('mongodb://127.0.0.1:27017/Polling_System');
-const dbURI = ('mongodb+srv://mahmood68:HsKmej0cQB7HQJ0p@cluster0.sl5apwz.mongodb.net/?retryWrites=true&w=majority')
+// method to connect to db
+async function main(){
+    try{
+        const db = await mongoose.connect('mongodb+srv://pankaj3112:ftHLwuuPqJvyE9Qj@cluster0.lm6iyo2.mongodb.net/?retryWrites=true&w=majority');
+        module.exports = db;
+        console.log('**** MongoDB Connected ****')
+    }
+    catch(err){
+        console.log("****Error in connecting to db ----> ", err);
+    }
+}
 
-// mongodb+srv://mahmood68:HsKmej0cQB7HQJ0p@cluster0.sl5apwz.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+main();
